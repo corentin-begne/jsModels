a04889b150633cf2d7ab46c58c64ef6c12629e76
corentin begne <corentin.begne@gmail.com>
2016-03-13 04:09:31 +0100

    more

--- manager.js
+++ manager.js
@@ -8,7 +8,7 @@ var ManagerModel;
     * @property {ActionModel} [action = new ActionModel()] Instance of ActionModel
     * @description Manage global manager functions and actions
     */
-    ManagerModel = function(){        
+    ManagerModel = function(cb){        
         var that = this;
         extendSingleton(ManagerModel);                
         this.container = $("body");
@@ -18,7 +18,14 @@ var ManagerModel;
         ], loaded);
 
         function loaded(){
-            that.actionModel = ActionModel.getInstance();
+            ActionModel.getInstance(loadedModel);
+
+            function loaded(instance){
+                that.actionModel = instance;
+                if(isDefined(cb)){
+                    cb(that);
+                }
+            }
         }
     };
 
@@ -27,8 +34,12 @@ var ManagerModel;
      * @description get the single class instance
      * @return {ManagerModel} the single class instance
      */
-     ManagerModel.getInstance = function(){
-        return getSingleton(ManagerModel);
+    ManagerModel.getInstance = function(cb){
+        if(isDefined(cb)){
+            getSingleton(ManagerModel, cb);
+        } else {
+            return getSingleton(ManagerModel);
+        }
     };
 
     ManagerModel.prototype.getVars = function(container){     
@@ -50,31 +61,41 @@ var ManagerModel;
         function addEvent(i, element){
             var data = $(element).is("[action-data]") ? $.parseJSON($(element).attr("action-data")) : {};
             if(isDefined(data.class)){
-                data.class = window[data.class].getInstance();
+                window[data.class].getInstance(loadedClass);
+                
+            }
+            init();
+
+            function loadedClass(instance){
+                data.class = instance;
                 if(isDefined(data.class.init)){
                     data.class.init();
                 }
-            }
-            if(!isDefined(data.type)){
-                data.type = "click";                    
-            }
-            if(data.type=== "init"){
-                that[data.fn](element, data, event);
-            } else {
-                $(element).unbind(data.type);
-                $(element).bind(data.type, sendEvent);
+                init();
             }
 
-            function sendEvent(event){                
-                data.fn = isDefined(data.fn) ? data.fn : "action";
-                if($(element).is("[preventDefault]")){
-                    event.preventDefault();
+            function init(){
+                if(!isDefined(data.type)){
+                    data.type = "click";                    
                 }
-                if($(element).is("[stopPropagation]")){
-                    event.stopPropagation();
+                if(data.type=== "init"){
+                    that[data.fn](element, data, event);
+                } else {
+                    $(element).unbind(data.type);
+                    $(element).bind(data.type, sendEvent);
                 }
-                if(isDefined(that[data.fn])){
-                    that[data.fn](element, data, event);                    
+
+                function sendEvent(event){                
+                    data.fn = isDefined(data.fn) ? data.fn : "action";
+                    if($(element).is("[preventDefault]")){
+                        event.preventDefault();
+                    }
+                    if($(element).is("[stopPropagation]")){
+                        event.stopPropagation();
+                    }
+                    if(isDefined(that[data.fn])){
+                        that[data.fn](element, data, event);                    
+                    }
                 }
             }
         }
