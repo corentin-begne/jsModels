39bda243ae001c9a3da698002a2755ace4e20acc
Corentin Bègne <corentin.begne@gmail.com>
2016-12-19 15:39:16 +0100

    more

--- manager.js
+++ manager.js
@@ -163,7 +163,7 @@ var ManagerModel;
         // get css and manager if not exists
         if(!isDefined(window[name+"Manager"])){
             loadCss(basePath+'css/'+data.path+"/main.css");
-            require([basePath+'js/'+data.path+"/manager.min.js"], ready);
+            require([basePath+'js/'+data.path+"/manager.min"], ready);
         } else {
             ready();
         }
