7bf766ad34309d38c07a59d6eab09cdc978b8ea8
corentin-begne <corentin.begne@gmail.com>
2018-01-16 16:15:42 +0100

    doc

--- action.js
+++ action.js
@@ -1,12 +1,11 @@
-/*global ActionHelper, extendSingleton, getSingleton */
+/*global ActionHelper, extendSingleton, getSingleton, isDefined */
 var ActionModel;
 (function(){
     "use strict";
     /**
     * @name ActionModel
-    * @description Manage all global manager actions
-    * @property {ActionHelper}    [action = new ActionHelper()]       Instance of ActionHelper
-    * @property {InterfaceHelper} [interface = new InterfaceHelper()] Instance of InterfaceHelper
+    * @description Manage all http request type
+    * @property {ActionHelper} [action = new ActionHelper()] Instance of ActionHelper
     * @constructor
     */
     ActionModel = function(cb){
@@ -42,11 +41,11 @@ var ActionModel;
     };
 
     /**
-     * @method ActionModel#sendData
-     * @description Send data to the manager action
-     * @param  {String}   [path] Action name
-     * @param  {Object}   [data] Data to send to the action
-     * @param  {Function} [cb]   Callback trigger on success with Object data
+     * @method ActionModel#getHtml
+     * @description Make an ajax call with response in plain text
+     * @param  {String}   [path] Route to call
+     * @param  {Object}   [data] Data to send in post
+     * @param  {Function} [cb]   Tiggered on complete with result in param
      */
     ActionModel.prototype.getHtml = function(path, data, cb){
         var options = {
@@ -59,11 +58,11 @@ var ActionModel;
     };
 
     /**
-     * @method ActionModel#sendData
-     * @description Send data to the manager action
-     * @param  {String}   [path] Action name
-     * @param  {Object}   [data] Data to send to the action
-     * @param  {Function} [cb]   Callback trigger on success with Object data
+     * @method ActionModel#getFormHtml
+     * @description Make an ajax call sending a form data with response in plain text
+     * @param  {String}   [path] Route to call
+     * @param  {FormData} [data] Form data to send in post
+     * @param  {Function} [cb]   Tiggered on complete with result in param
      */
     ActionModel.prototype.getFormHtml = function(path, data, cb){
         var options = {
@@ -77,8 +76,8 @@ var ActionModel;
     };
 
     /**
-     * @method ActionModel#sendData
-     * @description Send data to the manager action
+     * @method ActionModel#sendDataNoLoad
+     * @description Make ajax call without loader and with response in json
      * @param  {String}   [path] Action name
      * @param  {Object}   [data] Data to send to the action
      * @param  {Function} [cb]   Callback trigger on success with Object data
@@ -87,18 +86,47 @@ var ActionModel;
         this.sendData(path, data, cb, true);
     };
 
+    /**
+     * @method ActionModel#apiForm
+     * @description Make ajax api call sending form data
+     * @param  {String}   [path] Action name
+     * @param  {FormData} [data] Form data to send to the action
+     * @param  {Function} [cb]   Callback trigger on success with Object data
+     */
     ActionModel.prototype.apiForm = function(path, data, cb){
-        this.sendForm('api/'+path, data, cb);
+        this.sendForm("api/"+path, data, cb);
     };
 
+    /**
+     * @method ActionModel#api
+     * @description Make ajax api call
+     * @param  {String} [path] Action name
+     * @param  {Object} [data] Form data to send to the action
+     * @param  {Function} [cb]   Callback trigger on success with Object data
+     */
     ActionModel.prototype.api = function(path, data, cb){
-        this.sendData('api/'+path, data, cb);
+        this.sendData("api/"+path, data, cb);
     };
 
+    /**
+     * @method ActionModel#apiNoLoad
+     * @description Make ajax api call without loader
+     * @param  {String} [path] Action name
+     * @param  {Object} [data] Form data to send to the action
+     * @param  {Function} [cb]   Callback trigger on success with Object data
+     */
     ActionModel.prototype.apiNoLoad = function(path, data, cb){
-        this.sendData('api/'+path, data, cb, true);
+        this.sendData("api/"+path, data, cb, true);
     };
 
+    /**
+     * @method ActionModel#sendData
+     * @description Make ajax call with response in json
+     * @param  {String}   [path] Action name
+     * @param  {Object}   [data] Data to send to the action
+     * @param  {Function} [cb]   Callback trigger on success with Object data
+     * @param {Boolean} [noload] Specify if loader must be used or no
+     */
     ActionModel.prototype.sendData = function(path, data, cb, noload){
         var options = {
             type: "post",
@@ -114,10 +142,11 @@ var ActionModel;
 
     /**
      * @method ActionModel#sendData
-     * @description Send data to the manager action
+     * @description Make ajax call sending form data with response in json
      * @param  {String}   [path] Action name
-     * @param  {Object}   [data] Data to send to the action
+     * @param  {FormData} [data] Form data to send to the action
      * @param  {Function} [cb]   Callback trigger on success with Object data
+     * @param  {Boolean} [noload] Specify if loader must be used or no
      */
     ActionModel.prototype.sendForm = function(path, data, cb, noload){
         var options = {
